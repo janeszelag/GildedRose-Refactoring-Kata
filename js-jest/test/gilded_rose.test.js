8a40eeeb52ad2eab9c3f58eb8f47a56101c6bc4a
Katherine <katherine.szelag@mail.utoronto.ca>
2020-05-26 16:39:57 -0400

    testing aged brie

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -11,6 +11,32 @@ describe("Gilded Rose", function() {
   it("quality of an item is never negative", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
+    expect(items[0].quality).not.toBe(-1);
+  });
+
+  it("quality of an item decreases by 1 at the end of each day", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(0);
   });
+
+  it("sellIn of an item decreases by 1 at the end of each day", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toBe(0);
+  });
+
+  it("the quality of an item is never more than 50", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("quality of Aged Brie increases by 1 by one at the end of each day", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(2);
+  });
+
 });
+

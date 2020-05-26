bf6f2ee18f6c979154527bd4acead56318c83b56
Katherine <katherine.szelag@mail.utoronto.ca>
2020-05-26 13:44:21 -0400

    write test to check quality is not negative

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,16 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
+  it("name of item doesn't change", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
+  });
+
+
+  it("quality of an item is never negative", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
   });
 });
